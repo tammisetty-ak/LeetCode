b4135a846c74286e6dc72cb1c86e1ffb08443c97
Ashok Kumar <ashokkumar200194@gmail.com>
2025-04-22 18:26:33 -0400

    Time: 1 ms (70.51%), Space: 42 MB (80.23%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,47 +13,28 @@
  *     }
  * }
  */
-
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        
         Queue<TreeNode> queue = new LinkedList<>();
         List<Integer> res = new ArrayList<>();
-
         queue.offer(root);
-
         while(!queue.isEmpty()) {
             int len = queue.size();
-            TreeNode rightNode = null;
-            // List<Integer> temp = new ArrayList<Integer>();
-
-            while(len > 0) {
+            Integer temp = null;
+            while(len > 0){
                 TreeNode node = queue.poll();
-                if(node != null){
-                    // temp.add(node.val);
-                    rightNode = node;
+                if(node != null) {
+                    temp = node.val;
                     queue.offer(node.left);
                     queue.offer(node.right);
                 }
                 len--;
             }
-
-            if(rightNode != null){
-                res.add(rightNode.val);
+            if(temp != null) {
+                res.add(temp);
             }
-            // addLastElementToRes(temp, res);
         }
         return res;
     }
-
-    // private void addLastElementToRes(List<Integer> temp, List<Integer> res) {
-    //     int len = temp.size();
-
-    //     while(len > 0){
-    //         if(temp.get(len - 1) != null) {
-    //             res.add(temp.get(len-1));
-    //             break;
-    //         }
-    //         len--;
-    //     }
-    // }
 }
