8b7e0a2b196fe7311e8992d04a0c04dcb70c5197
Ashok Kumar <ashokkumar200194@gmail.com>
2025-04-26 00:16:41 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2408-design-sql/2408-design-sql.java
+++ 2408-design-sql/2408-design-sql.java
@@ -1,22 +1,22 @@
 class SQL {
 
     private class Table {
-        int autoId = 0;
-        int colSize;
+        int autoId;
         Map<Integer, List<String>> rows;
+        int colSize;
 
-        public Table(int colSize) {
-            this.autoId = 1;
-            this.colSize = colSize;
-            rows = new HashMap();
-
+        public Table(int columnSize) {
+            colSize = columnSize;
+            autoId = 0;
+            rows = new HashMap<>();
         }
     }
 
     Map<String, Table> tables;
 
     public SQL(List<String> names, List<Integer> columns) {
-        tables = new HashMap<>(names.size() , 1f);
+        tables = new HashMap<>();
+
         for(int i = 0; i < names.size(); i++) {
             tables.put(names.get(i), new Table(columns.get(i)));
         }
@@ -24,19 +24,21 @@ class SQL {
     
     public boolean ins(String name, List<String> row) {
         var table = tables.get(name);
+
         if(table == null || table.colSize != row.size()) {
             return false;
         }
 
-        table.rows.put(table.autoId++, row);
+        table.rows.put(++table.autoId, row);
         return true;
     }
     
     public void rmv(String name, int rowId) {
         var table = tables.get(name);
-        if(table == null) {
+        if(table == null || table.colSize < rowId) {
             return;
         }
+
         table.rows.remove(rowId);
     }
     
@@ -46,27 +48,31 @@ class SQL {
         if(table == null) {
             return "<null>";
         }
-        List<String> strings= table.rows.get(rowId);
+
+        List<String> strings = table.rows.get(rowId);
+
         if(strings == null || strings.size() < columnId) {
             return "<null>";
         }
+
         return strings.get(columnId - 1);
     }
     
     public List<String> exp(String name) {
         var table = tables.get(name);
-
+        List<String> res = new ArrayList();
+        
         if(table == null) {
-            return new ArrayList();
+            return res;
         }
-        var entrySet = table.rows.entrySet();
-        List<String> res = new LinkedList();
+        
+        var entryset = table.rows.entrySet();
 
-        for(var row: entrySet) {
-            res.add(row.getKey().toString() + "," + String.join(",", row.getValue()));
-            
+        for(var e: entryset) {
+            res.add(e.getKey().toString() + "," + String.join(",", e.getValue()));
         }
         return res;
+
     }
 }
 
