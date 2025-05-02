f0f944bf90b95d9e180abebb352bcabbe7f885da
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-02 02:39:11 -0400

    Time: 4 ms (99.58%), Space: 45.3 MB (79.77%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,31 +1,26 @@
 class Solution {
-    private boolean checkPalindrome(String s, int i, int j) {
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+
         while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
+            if(s.charAt(i) != s.charAt(j)) {
+                return (checkPalin(s, i, j - 1) || checkPalin(s, i + 1, j));
             }
-            
             i++;
             j--;
         }
-        
+
         return true;
     }
-    
-    public boolean validPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        
-        while (i < j) {
-            // Found a mismatched pair - try both deletions
-            if (s.charAt(i) != s.charAt(j)) {
-                return (checkPalindrome(s, i, j - 1) || checkPalindrome(s, i + 1, j));
+
+    private boolean checkPalin(String s, int i, int j) {
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
             }
-            
             i++;
             j--;
         }
-        
         return true;
     }
 }
