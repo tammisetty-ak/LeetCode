32ad710088042334d7e8c107c619d519cb2a9f71
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-11 02:27:12 -0400

    Time: 9 ms (48.58%), Space: 46.4 MB (83.61%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
 
-        Arrays.sort(intervals, (a, b) -> {
+        Arrays.sort(intervals, (a,b) -> {
             return a[0] - b[0];
         });
 
@@ -11,11 +11,11 @@ class Solution {
             if(merged.isEmpty() || merged.getLast()[1] < interval[0]) {
                 merged.add(interval);
             }
-            else{ 
-                merged.getLast()[1] = Math.max(interval[1], merged.getLast()[1]);
+            else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
             }
         }
 
-        return merged.toArray(new int[merged.size()][]);
+        return merged.toArray(new int[merged.size()][2]);
     }
 }
