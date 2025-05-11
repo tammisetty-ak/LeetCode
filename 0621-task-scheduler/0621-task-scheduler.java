964e1d6ec9a2e683e15621c300464f6be5bed0ad
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-11 00:05:34 -0400

    Time: 23 ms (50.94%), Space: 45.9 MB (43.84%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -2,35 +2,36 @@ class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] charCounts = new int[26];
 
-        for(char task: tasks) {
-            charCounts[task - 'A']++;
-        }
         int res = 0;
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for(char task : tasks) {
+            charCounts[task - 'A']++;
+        }
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
 
         for(int charCount: charCounts) {
             if(charCount > 0)
                 maxHeap.offer(charCount);
         }
 
+
         while(!maxHeap.isEmpty()) {
             int cycle = n + 1;
 
-
             List<Integer> store = new ArrayList();
             int taskCount = 0;
+
             while(cycle > 0 && !maxHeap.isEmpty()) {
                 int task = maxHeap.poll();
-                if(task > 1) {
-                    store.add(task - 1);
-                }
+                store.add(task - 1);
                 taskCount++;
                 cycle--;
             }
 
-            for(int s: store) {
-                maxHeap.offer(s);
+            for(int task: store) {
+                if(task > 0)
+                    maxHeap.offer(task);
             }
 
             res += maxHeap.isEmpty() ? taskCount : n + 1;
