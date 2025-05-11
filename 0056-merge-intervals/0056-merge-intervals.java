eda9d2888ac8ce427f673585be7f95442c1e9d99
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-11 02:22:09 -0400

    Time: 8 ms (86.75%), Space: 46.7 MB (38.6%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,20 +5,14 @@ class Solution {
             return a[0] - b[0];
         });
 
-        List<int[]> merged = new ArrayList();
-        int[] last = intervals[0];
-        merged.add(last);
+        LinkedList<int[]> merged = new LinkedList();
 
-        for(int i = 1; i < intervals.length; i++) {
-            int currStart = intervals[i][0];
-            int currEnd = intervals[i][1];
-
-            if(currStart <= last[1]) {
-                last[1] = Math.max(last[1], currEnd);
+        for(int[] interval: intervals) {
+            if(merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
             }
-            else{
-                last = intervals[i];
-                merged.add(last);
+            else{ 
+                merged.getLast()[1] = Math.max(interval[1], merged.getLast()[1]);
             }
         }
 
