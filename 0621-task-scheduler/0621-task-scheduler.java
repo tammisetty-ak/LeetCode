36335469065b6c430586232d1d05455c72d62896
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-11 00:16:25 -0400

    Time: 23 ms (50.94%), Space: 46.2 MB (18.4%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.java
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -1,17 +1,15 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int charCounts[] = new int[26];
-
+        int[] charCounts = new int[26];
         for(char task: tasks) {
             charCounts[task - 'A']++;
         }
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
-
         int res = 0;
-        
-        for(int charCount : charCounts) {
-            if (charCount > 0) {
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
+        for(int charCount: charCounts) {
+            if(charCount > 0) {
                 maxHeap.offer(charCount);
             }
         }
@@ -19,24 +17,26 @@ class Solution {
         while(!maxHeap.isEmpty()) {
             int cycle = n + 1;
 
-            List<Integer> store = new ArrayList();
-
             int taskCount = 0;
+            
+            List<Integer> store = new ArrayList();
 
             while(cycle > 0 && !maxHeap.isEmpty()) {
                 int task = maxHeap.poll();
-                store.add(task - 1);
+                if(task > 1) {
+                    store.add(task - 1);
+                }
                 taskCount++;
                 cycle--;
             }
 
-            for(int updatedTasks: store) {
-                if(updatedTasks > 0)
-                    maxHeap.offer(updatedTasks);
+            for(int updatedTask : store) {
+                maxHeap.offer(updatedTask);
             }
 
             res += maxHeap.isEmpty() ? taskCount : n + 1;
         }
+
         return res;
     }
 }
