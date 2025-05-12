c0750aba9e7b3934bdba3c2992143b9e958085fc
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-11 20:52:51 -0400

    Time: 3 ms (22.11%), Space: 55.4 MB (75.11%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,29 +1,44 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+
         int n = nums.length;
-
-        // int[] left = new int[n];
-        // int[] right = new int[n];
-
-
-        // left[0] = 1;
-        // right[n-1] = 1;
-
-        int[] ans = new int[n];
-
-        ans[0] = 1;
-
-        for(int i = 1; i < n; i++){
-            ans[i] = nums[i-1] * ans[i-1];
-        }
-        
-        int r = 1;
-
-        for(int i = n - 1; i >= 0; i--){
-            ans[i] = ans[i] * r;
-            r *= nums[i];
+        int[] res = new int[n];
+        Arrays.fill(res, 1);
+        for(int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
         }
 
-        return ans;
+        int prod = 1;
+
+        for(int i = n - 1; i >= 0; i--) {
+            res[i] = prod * res[i];
+            prod *= nums[i];
+        }
+
+        return res;
     }
 }
+
+// 1 2 3 4
+
+// res -> 1 1 2 6
+
+
+// res[0] = 1
+// res[1] = res[0] * nums[0]
+// res[2] = res[1] * nums[1]
+// res[3] = res[2] * nums[2]
+
+
+//         res[i] = res[i-1] * nums[i-1]
+
+// nums -> 1 2 3 4
+// res -> 1 1 2 6
+
+
+// i -> n - 1
+
+// prod - 1
+
+// res[i - 1] = prod * res[i-1] 
+// prod *= nums[i]
