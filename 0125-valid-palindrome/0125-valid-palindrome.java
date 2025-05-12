fdec5ba03be72cb0c1c2ceda3ca8211f60efa270
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-12 00:58:58 -0400

    Time: 2 ms (99.02%), Space: 43 MB (75.85%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0, end = s.length() - 1;
+
+        while(start < end) {
+            while(start < end && !Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            }
+
+            while(end > start && !Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            }
+
+            if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                return false;
+            }
+            else {
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+}
