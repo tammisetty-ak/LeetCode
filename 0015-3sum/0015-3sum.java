5eff6711fc0530a96f826628a0c0c660a0d49bad
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-12 22:02:12 -0500

    Time: 26 ms (91.99%), Space: 51.8 MB (44.6%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,31 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
+
+        List<List<Integer>> res = new ArrayList();
+
         for(int i = 0; i < nums.length; i++) {
-            if(i == 0 || nums[i] != nums[i - 1]) {
-                twoSumII(nums, i, res);
+            if(i == 0 || nums[i - 1] != nums[i]) {
+                twoSum(nums, i, res);
             }
         }
 
         return res;
     }
 
-    public void twoSumII(int nums[], int i, List<List<Integer>> res) {
+
+    private void twoSum(int[] nums, int i, List<List<Integer>> res) {
         int low = i + 1, high = nums.length - 1;
 
         while(low < high) {
-            int sum = nums[i] + nums[low] + nums[high];
+            int sum = nums[low] + nums[high] + nums[i];
 
             if(sum < 0) {
                 low++;
             }
-
-            else if (sum > 0) {
+            else if(sum > 0) {
                 high--;
             }
-            
             else {
                 res.add(Arrays.asList(nums[i], nums[low++], nums[high--]));
                 while(low < high && nums[low - 1] == nums[low]) {
@@ -34,4 +34,39 @@ class Solution {
             }
         }
     }
-}
+} 
+
+/*
+
+1 -1 -1 0
+
+-1 -1 0 1
+
+i == 0
+
+low = 1, high = 3
+
+1 < 3
+
+sum -> -1
+
+-1 < 0
+low++ -> 2
+
+sum -> 0
+
+res.add(-1 0 1)
+3 < 3 
+
+i == 1 
+
+i-1 != i
+
+
+i == 2
+
+i-1 == i
+
+low -> 3, high - 3
+
+*/
