f0d586be135c143e66cbf2fcf90431ac335f0bd3
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-13 22:59:00 -0500

    Time: 21 ms (94.76%), Space: 56.9 MB (85.83%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        int[] ans = new int[temperatures.length];
+
+        for(int i = 0; i < temperatures.length; i++) {
+            int currTemp = temperatures[i];
+
+            while(!stack.isEmpty() && temperatures[stack.peek()] < currTemp) {
+                int prevDay = stack.pop();
+                ans[prevDay] = i - prevDay;
+            }
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
