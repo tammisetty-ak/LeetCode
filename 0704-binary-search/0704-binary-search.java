9d19163a78f25ac083fd5d9ebe96929a69e7ea59
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-15 00:55:38 -0500

    Time: 0 ms (100%), Space: 45.9 MB (45.78%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,8 +1,8 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
+        int left = 0, right = nums.length;
 
-        while(left <= right) {
+        while(left < right) {
             int mid = left + (right - left) / 2;
             if(nums[mid] == target) {
                 return mid;
@@ -11,7 +11,7 @@ class Solution {
                 left = mid + 1;
             }
             else {
-                right = mid - 1;
+                right = mid;
             }
         }
 
