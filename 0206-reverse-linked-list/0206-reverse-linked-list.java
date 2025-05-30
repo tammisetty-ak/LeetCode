dbe5f93b4fe4162647e500913314de1f1bf3984e
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-29 22:50:19 -0500

    Time: 0 ms (100%), Space: 42.3 MB (82.22%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -23,3 +23,41 @@ class Solution {
         return prev;
     }
 }
+
+/*
+
+1 2 3 4 5 
+
+null 1 2 3 4 5 
+p - null, c - 1
+
+temp = curr.next
+t - 2 3 4 5
+t - 3 4 5
+t - 4 5
+t - 5
+t - null
+
+curr.next = prev;
+1 null
+2 1 null
+3 2 1 null
+4 3 2 1 null
+5 4 3 2 1 null
+
+
+prev = curr
+p - 1 null
+p - 2 1 null
+p - 3 2 1 null
+p - 4 3 2 1 null
+p - 5 4 3 2 1 null
+
+curr = temp;
+curr - 2 3 4 5
+curr - 3 4 5
+curr - 4 5
+curr - 5
+curr - null
+
+*/
