742b62084c6ff60e427d324c1f7c891e160f1526
Ashok Kumar <ashokkumar200194@gmail.com>
2025-05-31 12:11:50 -0500

    Time: 1 ms (54.86%), Space: 41.6 MB (9.27%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,11 +1,24 @@
 class Solution {
     public boolean isUgly(int n) {
-        while(n > 1) {
-            if( n % 2 == 0) n /= 2;
-            else if (n % 3 == 0) n /= 3;
-            else if (n % 5 == 0) n /= 5;
-            else return false;
+        // A non-positive integer cannot be ugly
+        if (n <= 0) {
+            return false;
         }
+
+        // Factorize by dividing with permitted factors
+        for (int factor : new int[] { 2, 3, 5 }) {
+            n = keepDividingWhenDivisible(n, factor);
+        }
+
+        // Check if the integer is reduced to 1 or not.
         return n == 1;
+    }
+
+    // Keep dividing dividend by divisor when division is possible.
+    private int keepDividingWhenDivisible(int dividend, int divisor) {
+        while (dividend % divisor == 0) {
+            dividend /= divisor;
+        }
+        return dividend;
     }
 }
