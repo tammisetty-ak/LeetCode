90b8bcd1f0fb333bc40ad20d17d30ff469fbd42e
Ashok Kumar <ashokkumar200194@gmail.com>
2025-06-27 17:07:37 -0500

    Time: 1 ms (99.91%), Space: 42.4 MB (23.66%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.java
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -4,15 +4,15 @@ class Solution {
         int maxPow = Integer.MIN_VALUE, count = 1;
         for(char c: s.toCharArray()) {
             if(c != candidate) {
-                maxPow = Math.max(maxPow, count);
                 candidate = c;
                 count = 1;
             }
             else {
                 count++;
             }
+            maxPow = Math.max(maxPow, count);
         }
 
-        return Math.max(maxPow, count);
+        return maxPow;
     }
 }
