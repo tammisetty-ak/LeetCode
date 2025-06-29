180609e16f55e09744a1bac1fdc897cf71549a91
Ashok Kumar <ashokkumar200194@gmail.com>
2025-06-28 21:03:00 -0500

    Time: 4 ms (98.7%), Space: 45 MB (42.4%) - LeetHub

--- 1086-high-five/1086-high-five.java
+++ 1086-high-five/1086-high-five.java
@@ -1,31 +1,32 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-
+        Map<Integer, PriorityQueue> map = new HashMap();
         for(int[] item: items) {
             int id = item[0];
-            PriorityQueue<Integer> queue = map.getOrDefault(id, new PriorityQueue<>());
-            queue.offer(item[1]);
-            if(queue.size() > 5) {
-                queue.poll();
-            }
-            map.put(id, queue);
-        }
+            PriorityQueue<Integer> minHeap = map.getOrDefault(id, new PriorityQueue());
 
+            minHeap.offer(item[1]);
+            if(minHeap.size() > 5) {
+                minHeap.poll();
+            }
+            map.put(id, minHeap);
+        }
         int[][] res = new int[map.size()][2];
         int i = 0;
-
-        for(int key : map.keySet()) {
-            int top = 0;
+        for(int key: map.keySet()) {
+            int[] temp = new int[2];
+            temp[0] = key;
+            PriorityQueue<Integer> queue = map.get(key);
             int sum = 0;
-            PriorityQueue <Integer> queue = map.get(key);
             while(!queue.isEmpty()) {
                 sum += queue.poll();
             }
-            int[] temp = { key, sum / 5 };
+            temp[1] = sum / 5;
+
             res[i++] = temp;
         }
 
         return res;
+
     }
 }
