d8d781db51a1c5e805b608d13d6a1f833a40c6b4
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-06 16:07:01 -0500

    Time: 1 ms (99.76%), Space: 46.4 MB (22.78%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,33 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums.length > 1) {
-            int i = 0, j = 1;
+        int insertPos = 0;
 
-            while (j < nums.length) {
-                if (nums[i] == 0) {
-                    // Move j to the next non-zero
-                    while (j < nums.length && nums[j] == 0) {
-                        j++;
-                    }
-
-                    // Only swap if j is still within bounds
-                    if (j < nums.length) {
-                        nums[i] = nums[j];
-                        nums[j] = 0;
-                    }
-                }
-
-                // If current i is not zero, just move i forward
-                // Else wait until we’ve done the swap, and then move
-                if (nums[i] != 0) {
-                    i++;
-                } else if (j > i) {
-                    i++;
-                }
-
-                // Always make sure j is ahead of i
-                j = Math.max(j, i + 1);
+        // First pass: Move all non-zero elements to the front
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
             }
+        }
+
+        // Second pass: Fill the remaining positions with zeros
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
         }
     }
 }
