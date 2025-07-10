df115a5c23a87c02722aa5efe728a5f95bc86d00
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-10 15:36:09 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,27 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length) {
+            findMedianSortedArrays(nums2, nums1);
+        }
+
         int m = nums1.length, n = nums2.length;
 
-        int i = 0, j = 0, k = 0;
-        int nums3[] = new int[m + n];
+        int left = 0, right = m;
 
-        while(i < m && j < n) {
-            if(nums1[i] < nums2[j]) {
-                nums3[k++] = nums1[i++];
+        while(left <= right) {
+            int partitionA = (left + right) / 2;
+            int partitionB = (m + n + 1) / 2 - partitionA;
+
+            int maxLeftA = (partitionA == 0) ? Integer.MIN_VALUE : nums1[partitionA - 1];
+            int minRightA = (partitionA == m) ? Integer.MAX_VALUE : nums1[partitionA];
+            int maxLeftB = (partitionB == 0) ? Integer.MIN_VALUE : nums2[partitionB - 1];
+            int minRightB = (partitionB == n) ? Integer.MAX_VALUE : nums2[partitionB];
+
+            if(maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if((m + n) % 2 == 0) {
+                    return (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2.0;
+                }
+                else {
+                    return Math.max(maxLeftA, maxLeftB);
+                }
+            }
+
+            else if(maxLeftA > minRightB) {
+                right = partitionA - 1;
             }
             else {
-                nums3[k++] = nums2[j++];
+                left = partitionA + 1;
             }
         }
-        
-        while (i < m) {
-            nums3[k++] = nums1[i++];
-        }
-
-        while(j < n) {
-            nums3[k++] = nums2[j++];
-        }
-
-        return nums3.length % 2 != 0 ? nums3[(m + n) / 2] : (nums3[(m + n) / 2] + nums3[((m + n) / 2) - 1]) / 2.0;
+        return 0.0;
     }
 }
