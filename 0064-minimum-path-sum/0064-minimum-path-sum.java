a20772dab7638d8ccaaaef33403dfc6c6fec2ad6
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-10 00:48:20 -0500

    Time: 7 ms (5.11%), Space: 48.2 MB (8.18%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,17 +1,19 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
+        int len = grid.length, width = grid[0].length;
 
-        for(int i = grid.length - 1; i >= 0; i--) {
-            for(int j = grid[0].length - 1; j >= 0; j--) {
-                if(i != grid.length - 1 && j == grid[0].length - 1) {
-                    dp[i][j] = grid[i][j] + dp[i + 1][j];
-                }
-                else if(i == grid.length - 1 && j != grid[0].length - 1) {
+        int[][] dp = new int[len][width];
+
+        for(int i = len - 1; i >= 0; i--) {
+            for(int j = width - 1; j >= 0; j--) {
+                if(i == len - 1 && j != width - 1) {
                     dp[i][j] = grid[i][j] + dp[i][j + 1];
                 }
-                else if(i != grid.length - 1 && j != grid[0].length - 1) {
-                    dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
+                else if(i != len - 1 && j == width - 1) {
+                    dp[i][j] =  grid[i][j] + dp[i + 1][j];
+                }
+                else if(i != len - 1 && j != width - 1) {
+                    dp[i][j] = Math.min(grid[i][j] + dp[i + 1][j], grid[i][j] + dp[i][j + 1]);
                 }
                 else {
                     dp[i][j] = grid[i][j];
