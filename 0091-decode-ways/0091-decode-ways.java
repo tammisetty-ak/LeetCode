b4252a32f6d6c15d5c74968848f8593b87013147
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-11 02:21:38 -0500

    Time: 1 ms (83.83%), Space: 41.9 MB (49.83%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,18 +1,21 @@
 class Solution {
     public int numDecodings(String s) {
-        int dp[] = new int[s.length() + 1];
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[s.length() + 1];
 
         dp[0] = 1;
-
-        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
 
         for(int i = 2; i < dp.length; i++) {
             if(s.charAt(i - 1) != '0') {
-                dp[i] = dp[i - 1];
+                dp[i] += dp[i - 1];
             }
-
             int twoDigit = Integer.valueOf(s.substring(i - 2, i));
-            if(twoDigit >= 10 && twoDigit <= 26) {
+
+            if(twoDigit >= 10 && twoDigit < 27) {
                 dp[i] += dp[i - 2];
             }
         }
