b11f27f1b0fe7043523bf4f5b8ce2d0e2a79772d
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-10 23:35:03 -0500

    Time: 47 ms (54.3%), Space: 116.6 MB (26.72%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,69 +1,69 @@
 class LRUCache {
 
-    public class ListNode {
+    class Node{
         int key;
         int val;
-        ListNode next;
-        ListNode prev;
+        Node next;
+        Node prev;
 
-        public ListNode(int key, int val) {
+        public Node(int key, int val) {
             this.key = key;
             this.val = val;
         }
     }
 
+    Node head, tail;
+
+    Map<Integer, Node> keyNodeMap;
     int capacity;
-    ListNode head, tail;
-    Map<Integer, ListNode> map;
 
     public LRUCache(int capacity) {
-        head = new ListNode(-1, -1);
-        tail = new ListNode(-1, -1);
-
-        map = new HashMap();
         this.capacity = capacity;
+        keyNodeMap = new HashMap();
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
         head.next = tail;
         tail.prev = head;
     }
     
     public int get(int key) {
-        if(!map.containsKey(key)) {
+        if(!keyNodeMap.containsKey(key)) {
             return -1;
         }
-        ListNode node = map.get(key);
+        Node node = keyNodeMap.get(key);
         removeNode(node);
         addNode(node);
         return node.val;
     }
     
     public void put(int key, int value) {
-        if(map.containsKey(key)) {
-            removeNode(map.get(key));
-            map.remove(key);
+        if(keyNodeMap.containsKey(key)) {
+            removeNode(keyNodeMap.get(key));
         }
-
-        ListNode node = new ListNode(key, value);
-        map.put(key, node);
+        Node node = new Node(key, value);
+        keyNodeMap.put(key, node);
         addNode(node);
-        if(map.size() > capacity) {
-            ListNode nodeToBeDeleted = head.next;
+        if(keyNodeMap.size() > capacity) {
+            Node nodeToBeDeleted = head.next;
             removeNode(nodeToBeDeleted);
-            map.remove(nodeToBeDeleted.key);
+            keyNodeMap.remove(nodeToBeDeleted.key);
         }
     }
 
-    public void removeNode(ListNode node) {
+    private void removeNode(Node node) {
         node.next.prev = node.prev;
         node.prev.next = node.next;
     }
 
-    public void addNode(ListNode node) {
-        ListNode prevTail = tail.prev;
-        prevTail.next = node;
-        node.prev = prevTail;
-        node.next = tail;
+    private void addNode(Node node) {
+        Node temp = tail.prev;
+        node.prev = temp;
+        temp.next = node;
         tail.prev = node;
+        node.next = tail;
     }
+
 }
 
 /**
