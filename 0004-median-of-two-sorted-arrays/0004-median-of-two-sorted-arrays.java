8c3186effb8c1fdca58e648bcc4ce08591145357
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-11 12:30:42 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,25 +1,24 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
         if(nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
-        int m = nums1.length, n = nums2.length;
 
+        int m = nums1.length, n = nums2.length;
 
         int left = 0, right = m;
 
         while(left <= right) {
-            int partitionA = (left + right) / 2;
-            int partitionB = (m + n + 1) / 2 - partitionA;
+            int partitionA = (left + right) / 2; // 0
+            int partitionB = (m + n + 1) / 2 - partitionA; // 1
 
-            int maxLeftA = (partitionA == 0) ? Integer.MIN_VALUE : nums1[partitionA - 1];
-            int minRightA = (partitionA == m) ? Integer.MAX_VALUE : nums1[partitionA];
-            int maxLeftB = (partitionB == 0) ? Integer.MIN_VALUE : nums2[partitionB - 1];
-            int minRightB = (partitionB == n) ? Integer.MAX_VALUE : nums2[partitionB];
-// 1 2 3 4 
-            if(maxLeftA <= minRightB && maxLeftB <= minRightA) {
-                if((m + n) % 2 == 0) {
+            int maxLeftA = (partitionA == 0) ? Integer.MIN_VALUE : nums1[partitionA - 1]; // min
+            int minRightA = (partitionA == m) ? Integer.MAX_VALUE : nums1[partitionA]; // 1
+            int maxLeftB = (partitionB == 0) ? Integer.MIN_VALUE : nums2[partitionB - 1]; // 1
+            int minRightB = (partitionB == n) ? Integer.MAX_VALUE : nums2[partitionB]; // max
+
+            if(maxLeftA < minRightB && maxLeftB < minRightA) { // min < 1 √ min < 1 
+                if((m + n) % 2 == 0) { 
                     return (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2.0;
                 }
                 else {
