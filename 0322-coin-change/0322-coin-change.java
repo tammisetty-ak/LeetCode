a8e15070e53f5163e6936d996fb7dc4158b59846
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-10 21:34:02 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,18 +1,23 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int count = 0;
-        Arrays.sort(coins);
-        while(amount > 0){
-            if(amount < coins[0]) {
-                return -1;
-            }
-            for(int i = coins.length - 1; i >= 0; i--) {
-                if(amount >= coins[i]) {
-                    count += (amount / coins[i]);
-                    amount = amount - ((amount / coins[i]) * coins[i]);
-                }
-            }
+
+    private int findLowestCoins(int[] coins, int amount, int index) {
+        if(amount == 0) {
+            return 0;
         }
-        return count;
+        if(index == coins.length || amount < 0) {
+            return Integer.MAX_VALUE - 1;
+        }
+
+        int take = Integer.MAX_VALUE;
+        if(coins[index] <= amount) {
+            take = 1 + findLowestCoins(coins, amount - coins[index], index);
+        }
+        int skip = findLowestCoins(coins, amount, index + 1);
+
+        return Math.min(skip, take);
+    }
+    public int coinChange(int[] coins, int amount) {
+        int res = findLowestCoins(coins, amount, 0);
+        return (res == Integer.MAX_VALUE - 1) ? -1 : res;
     }
 }
