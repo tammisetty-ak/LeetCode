5caeaee03ce1b4a258b20e06e0d60a9af060dc9c
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-10 23:23:43 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while(tx >= sx && ty >= sy) {
+            if(tx == sx && ty == sy) {
+                return true;
+            }
+            if(tx > ty) {
+                tx -= ty;
+            }
+            else {
+                ty -= tx;
+            }
+        }
+        return false;
+    }
+}
