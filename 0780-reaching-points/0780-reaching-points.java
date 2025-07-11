1b6a09ebb60b393cf57ad21990b62527a421e4e1
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-10 23:26:59 -0500

    Time: 0 ms (100%), Space: 41.1 MB (6.33%) - LeetHub

--- 0780-reaching-points/0780-reaching-points.java
+++ 0780-reaching-points/0780-reaching-points.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean reachingPoints(int sx, int sy, int tx, int ty) {
-        while(tx >= sx && ty >= sy) {
-            if(tx == sx && ty == sy) {
-                return true;
-            }
+        while(sx < tx && sy < ty) {
             if(tx > ty) {
-                tx -= ty;
+                tx %= ty;
             }
             else {
-                ty -= tx;
+                ty %= tx;
             }
         }
-        return false;
+
+        if(sx == tx && sy <= ty && (ty - sy) % sx == 0) {
+            return true;
+        }
+        return sy == ty && sx <= tx && (tx - sx) % sy == 0;
     }
 }
