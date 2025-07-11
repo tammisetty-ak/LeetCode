a4b7ea7f28156f033db3bdb9cb090a6bc24f0190
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-10 22:29:59 -0500

    Time: 4 ms (99.07%), Space: 44.8 MB (89.64%) - LeetHub

--- 1086-high-five/1086-high-five.java
+++ 1086-high-five/1086-high-five.java
@@ -1,32 +1,32 @@
 class Solution {
     public int[][] highFive(int[][] items) {
-        int k = 5;
-        Map<Integer, PriorityQueue<Integer>> map = new TreeMap();
-        for(int[] item: items) {
-            int id = item[0];
-            PriorityQueue<Integer> minHeap = map.getOrDefault(id, new PriorityQueue());
+        HashMap<Integer, Queue<Integer>> studentIdToScoresMap = new HashMap();
 
+        for(int[] item: items) {
+            int studentId = item[0];
+            if(!studentIdToScoresMap.containsKey(studentId)) {
+                studentIdToScoresMap.put(studentId, new PriorityQueue<Integer>());
+            }
+            Queue<Integer> minHeap = studentIdToScoresMap.get(studentId);
             minHeap.offer(item[1]);
-            if(minHeap.size() > k) {
+            if(minHeap.size() > 5) {
                 minHeap.poll();
             }
-            map.put(id, minHeap);
         }
-        int[][] res = new int[map.size()][2];
-        int i = 0;
-        for(int id: map.keySet()) {
-            PriorityQueue<Integer> queue = map.get(id);
-            int sum = 0;
-            while(!queue.isEmpty()) {
-                sum += queue.poll();
+        
+        int[][] res = new int[studentIdToScoresMap.size()][2];
+        int index = 0, sum = 0;
+        for(int studentId: studentIdToScoresMap.keySet()) {
+            res[index][0] = studentId;
+            sum = 0;
+            Queue<Integer> minHeap = studentIdToScoresMap.get(studentId);
+            while(!minHeap.isEmpty()) {
+                sum += minHeap.poll();
             }
-
-            res[i][0] = id;
-            res[i][1] = sum / k;
-            i++;
+            res[index][1] = sum / 5;
+            index++;
         }
 
         return res;
-
     }
 }
