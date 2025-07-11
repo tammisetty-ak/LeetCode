385c36d3f535b43705e51a9e9e510dac878b6dc9
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-11 09:54:36 -0500

    Time: 6 ms (64.89%), Space: 45.9 MB (6.2%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -4,33 +4,28 @@ class Solution {
 
         for(int asteroid : asteroids) {
             boolean alive = true;
-                while(!stack.isEmpty() && stack.peek() > 0 && asteroid < 0) {
-                    int prev = Math.abs(stack.peek());
-                    if(prev < Math.abs(asteroid)) {
-                        stack.pop();
-                    }
-                    else if(prev == Math.abs(asteroid)) {
-                        stack.pop();
-                        alive = false;
-                        break;
-
-                    }
-                    else {
-                        alive = false;
-                        break;
-                    }
+            while(!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+                if(stack.peek() < Math.abs(asteroid)) {
+                    stack.pop();
                 }
-            if(alive) stack.push(asteroid);
+                else if(stack.peek() == Math.abs(asteroid)) {
+                    stack.pop();
+                    alive = false;
+                    break;
+                }
+                else {
+                    alive = false;
+                    break;
+                }
+            }
+            if(alive) {
+                stack.push(asteroid);
+            }
         }
-
-       int[] res = new int[stack.size()];
-
-       for(int i = res.length - 1; i >= 0; i--) {
-        res[i] = stack.pop();
-       }
-
-       return res;
-
-        // return intArray;
+        int[] res = new int[stack.size()];
+        for(int i = res.length - 1; i >= 0; i--) {
+            res[i] = stack.pop();
+        }
+        return res;
     }
 }
