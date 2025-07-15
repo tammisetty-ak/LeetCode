d31adf54af253c8259c9ea64a14a63d96cb6b01c
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-14 22:18:35 -0500

    Time: 8 ms (32.81%), Space: 44.3 MB (71.05%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,37 +1,41 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        Map<Integer, List<int[]>> graph = new HashMap();
-        for(int[] i : flights) {
-            graph.computeIfAbsent(i[0], value -> new ArrayList<>()).add(new int[]{i[1], i[2]});
+        HashMap<Integer, List<int[]>> graph = new HashMap();
+        int stops = 0;
+        int dist[] = new int[n];
+
+        for (int[] flight : flights) {
+            graph.computeIfAbsent(flight[0], value -> new ArrayList()).add(new int[] { flight[1], flight[2] });
         }
 
-        int[] dist = new int[n];
         Arrays.fill(dist, Integer.MAX_VALUE);
 
-        Queue<int[]> queue = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList();
 
-        queue.offer(new int[]{src, 0});
-        int stops = 0;
+        queue.offer(new int[] { src, 0 });
 
-        while(stops <= k && !queue.isEmpty()) {
-            int sz = queue.size();
-            while(sz-- > 0) {
-                int[] temp = queue.poll();
-                int node = temp[0];
-                int distance = temp[1];
+        while (stops <= k && !queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int[] curr = queue.poll();
+                int currNode = curr[0];
+                int currPrice = curr[1];
 
-                if(!graph.containsKey(node)) {
+                if (!graph.containsKey(currNode)) {
                     continue;
                 }
 
-                for(int[] neighbor : graph.get(node)) {
-                    int neigh = neighbor[0];
-                    int price = neighbor[1];
-                    if(price + distance >= dist[neigh]) {
+                for (int[] neighborDetails : graph.get(currNode)) {
+                    int neighborNode = neighborDetails[0];
+                    int neighborPrice = neighborDetails[1];
+
+                    if (neighborPrice + currPrice >= dist[neighborNode]) {
                         continue;
                     }
-                    dist[neigh] = price + distance;
-                    queue.offer(new int[] {neigh, dist[neigh]});
+
+                    dist[neighborNode] = currPrice + neighborPrice;
+
+                    queue.offer(new int[] { neighborNode, dist[neighborNode] });
                 }
             }
             stops++;
