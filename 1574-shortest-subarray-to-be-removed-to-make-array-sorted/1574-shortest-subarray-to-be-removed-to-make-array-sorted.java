0ce64492f9eb88a85542d57d4c97d683714fe298
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-17 16:33:34 -0500

    Time: 1 ms (100%), Space: 65.3 MB (24.64%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.java
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.java
@@ -1,32 +1,31 @@
 class Solution {
     public int findLengthOfShortestSubarray(int[] arr) {
+        int n = arr.length;
+
+        // Step 1: Find longest non-decreasing prefix
         int left = 0;
-        int right = arr.length - 1;
-
-        for(; left < arr.length - 1; left++) {
-            if(arr[left] > arr[left + 1]) {
-                break;
-            }
+        while (left + 1 < n && arr[left] <= arr[left + 1]) {
+            left++;
         }
 
-        if(left == right) {
-            return 0;
+        if (left == n - 1) return 0; // Already sorted
+
+        // Step 2: Find longest non-decreasing suffix
+        int right = n - 1;
+        while (right > 0 && arr[right - 1] <= arr[right]) {
+            right--;
         }
 
-        for(; right > 0; right--) {
-            if(arr[right] < arr[right - 1]) {
-                break;
-            }
-        }
+        // Step 3: Try removing entire prefix or suffix
+        int result = Math.min(n - left - 1, right);
 
-        int result = Math.min(arr.length - left - 1, right);
-
-        for(int i = 0, j = right; i <= left && j < arr.length; ) {
-            if(arr[i] <= arr[j]) {
+        // Step 4: Try merging prefix and suffix
+        int i = 0, j = right;
+        while (i <= left && j < n) {
+            if (arr[i] <= arr[j]) {
                 result = Math.min(result, j - i - 1);
                 i++;
-            }
-            else {
+            } else {
                 j++;
             }
         }
