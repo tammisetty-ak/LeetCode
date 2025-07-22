3192d50c4cb35fdd827ff7d6e8ac90945364b85c
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-22 03:09:08 -0500

    Time: 1101 ms (5.18%), Space: 0B (100%) - LeetHub

--- 2893-calculate-orders-within-each-interval/2893-calculate-orders-within-each-interval.sql
+++ 2893-calculate-orders-within-each-interval/2893-calculate-orders-within-each-interval.sql
@@ -1,12 +1,6 @@
 # Write your MySQL query statement below
 
-With Intervals as (
-    select *, ceil(minute / 6) as interval_no
-    from Orders
-)
-
-
-select interval_no, sum(order_count) as total_orders
-from intervals
-group by interval_no
-order by interval_no
+select intervals.interval_no, sum(order_count) as total_orders
+from (select *, ceil(minute / 6) as interval_no from orders) as intervals
+group by intervals.interval_no
+order by intervals.interval_no
