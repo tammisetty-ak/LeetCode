6ba1e447d1f725bdb32044aa914f2318d56e8da3
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-23 04:13:23 -0500

    Time: 1 ms (56.45%), Space: 44.3 MB (71.41%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -1,48 +1,28 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-    
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
-
 class Solution {
     public Node connect(Node root) {
-        if(root == null) {
-            return null;
-        }
+        if (root == null) return null;
 
-        Node leftmost = root;
+        Node current = root; // start with the root
 
-        while(leftmost.left != null) {
-            Node head = leftmost;
+        while (current != null) {
+            Node dummy = new Node(0); // dummy node to build the next level
+            Node tail = dummy; // tail tracks the last node in the next level
 
-            while(head != null) {
-                head.left.next = head.right;
-
-                if(head.next != null) {
-                    head.right.next = head.next.left;
+            while (current != null) {
+                if (current.left != null) {
+                    tail.next = current.left;
+                    tail = tail.next;
                 }
 
-                head = head.next;
+                if (current.right != null) {
+                    tail.next = current.right;
+                    tail = tail.next;
+                }
+
+                current = current.next; // move to the next node in the current level
             }
 
-            leftmost = leftmost.left;
+            current = dummy.next; // move to the first node of the next level
         }
 
         return root;
