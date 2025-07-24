59da8c4574336a36309ff59364184c1eea6f3991
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-24 04:22:38 -0500

    Time: 2 ms (99.32%), Space: 54.2 MB (34.91%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,44 +1,18 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-
-        int[][] graph = new int[n][n];
-        
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(graph[i], 0);
-        }
+        int[] trusts = new int[n + 1];
 
         for(int[] t : trust) {
-            graph[t[0] - 1][t[1] - 1]++;
+            trusts[t[0]]--;
+            trusts[t[1]]++;
         }
 
-        boolean canBeJudge = true;
-        int count = 0;
-        int isJudge = -1;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(graph[i][j] != 0) {
-                    canBeJudge = false;
-                }
-            }
-            if(canBeJudge) {
-                count++;
-                if(count > 1) {
-                    return -1;
-                }
-                isJudge = i + 1;
+        for(int i = 1; i <= n; i++) {
+            if(trusts[i] == n - 1) {
+                return i;
             }
         }
 
-        for(int i = 0; i < n; i++) {
-            if(isJudge == -1 || graph[i][isJudge - 1] == 0) {
-                return -1;
-            }
-        }
-
-        return isJudge;
-
-
-       
+        return -1;
     }
 }
