5aceaf7ed025a517d1efa01b52254ea7883c85dd
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-23 23:14:38 -0500

    Time: 9 ms (96.1%), Space: 45.8 MB (43.15%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -8,49 +8,43 @@ class Solution {
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 if(grid[i][j] == 1) {
-                    dp[i][j] = 0;
+                    grid[i][j] = 0;
                 }
                 else {
-                    dp[i][j] = INF;
+                    grid[i][j] = INF;
                 }
             }
         }
 
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
-                if(dp[i][j] != 0) {
+                if(grid[i][j] != 0) {
                     if (i > 0) {
-                        dp[i][j] = Math.min(1 + dp[i - 1][j], dp[i][j]);
+                        grid[i][j] = Math.min(1 + grid[i - 1][j], grid[i][j]);
                     }
                     if(j > 0) {
-                        dp[i][j] = Math.min(1 + dp[i][j - 1], dp[i][j]);
+                        grid[i][j] = Math.min(1 + grid[i][j - 1], grid[i][j]);
                     }
                 }
             }
         }
+        int max = Integer.MIN_VALUE;
 
         for(int i = rows - 1; i >= 0; i--) {
             for(int j = cols - 1; j >= 0; j--) {
-                if(dp[i][j] != 0) {
+                if(grid[i][j] != 0) {
                     if(i < rows - 1) {
-                        dp[i][j] = Math.min(1 + dp[i + 1][j], dp[i][j]);
+                        grid[i][j] = Math.min(1 + grid[i + 1][j], grid[i][j]);
                     }
                     if(j < cols - 1) {
-                        dp[i][j] = Math.min(1 + dp[i][j + 1], dp[i][j]);
+                        grid[i][j] = Math.min(1 + grid[i][j + 1], grid[i][j]);
                     }
+                    if(grid[i][j] != INF) max = Math.max(grid[i][j], max);
                 }
             }
         }
 
-        int max = Integer.MIN_VALUE;
 
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(grid[i][j] == 0 && dp[i][j] != INF) {
-                    max = Math.max(dp[i][j], max);
-                }
-            }
-        }
 
         return max == Integer.MIN_VALUE ? -1 : max;
     }
