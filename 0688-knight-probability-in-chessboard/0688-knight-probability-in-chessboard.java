63827d76b3884ce71994d41cda696caed72fa293
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-25 03:13:03 -0500

    Time: 5 ms (89.16%), Space: 44.1 MB (27.71%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,35 +1,36 @@
 class Solution {
+    int[] dx = {1, 2, 1, 2, -1, -2, -1, -2};
+    int[] dy = {2, 1, -2, -1, 2, 1, -2, -1};
+
     public double knightProbability(int n, int k, int row, int column) {
-        int[] dx = {1, 2, 1, 2, -1, -2, -1, -2};
-        int[] dy = {2, 1, -2, -1, 2, 1, -2, -1};
+        Double memo[][][] = new Double[k + 1][n][n];
 
-        double[][][] dp = new double[k + 1][n][n];
+        return dfs(n, k, row, column, memo);
+    }
 
-        dp[0][row][column] = 1;
-
-        for(int move = 1; move <= k; move++) {
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    if(dp[move - 1][i][j] > 0) {
-                        for(int d = 0; d < 8; d++) {
-                            int ni = i + dx[d], nj = j + dy[d];
-                            if(ni >= 0 && ni < n && nj >= 0 && nj < n) {
-                                dp[move][ni][nj] += dp[move - 1][i][j] / 8.0;
-                            }
-                        }
-                    }
-                }
-            }
+    private double dfs(int n, int k, int row, int column, Double[][][] memo) {
+        if(row < 0 || row >= n || column < 0 || column >= n) {
+            return 0.0;
         }
 
-        double probability = 0.0;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                probability += dp[k][i][j];
-            }
+        if(k == 0) {
+            return 1.0;
         }
 
-        return probability;
+        if(memo[k][row][column] != null) {
+            return memo[k][row][column];
+        }
+
+        double prob = 0.0;
+
+        for(int d = 0; d < 8; d++) {
+            int ni = row + dx[d];
+            int nj = column + dy[d];
+            prob += dfs(n, k - 1, ni, nj, memo) / 8.0;
+        }
+
+        memo[k][row][column] = prob;
+
+        return prob;
     }
 }
