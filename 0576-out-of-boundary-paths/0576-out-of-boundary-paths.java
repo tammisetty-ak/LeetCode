90f6a5067c3e820beb0537d70a19892ad4441d99
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-25 02:30:52 -0500

    Time: 6 ms (33.1%), Space: 42.3 MB (58.56%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int MOD = 1000000007;
+        int[][][] dp = new int[maxMove + 1][m][n];
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        dp[0][startRow][startColumn] = 1;
+        int result = 0;
+
+        for (int move = 1; move <= maxMove; move++) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dp[move - 1][i][j] > 0) {
+                        for (int d = 0; d < 4; d++) {
+                            int ni = i + dx[d], nj = j + dy[d];
+                            if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                                dp[move][ni][nj] = (dp[move][ni][nj] + dp[move - 1][i][j]) % MOD;
+                            } else {
+                                result = (result + dp[move - 1][i][j]) % MOD;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
