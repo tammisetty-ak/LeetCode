343a594b9ff00d04074b32e43fe5dce9d3248185
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-26 02:49:34 -0500

    Time: 494 ms (83.25%), Space: 0B (100%) - LeetHub

--- 2314-the-first-day-of-the-maximum-recorded-degree-in-each-city/2314-the-first-day-of-the-maximum-recorded-degree-in-each-city.sql
+++ 2314-the-first-day-of-the-maximum-recorded-degree-in-each-city/2314-the-first-day-of-the-maximum-recorded-degree-in-each-city.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
 
-select city_id, min(day) as day, degree
-from Weather
-where (city_id, degree) in (
-select city_id, max(degree)
+select w.city_id as city_id, min(w.day) as day, w.degree as degree
+from Weather w JOIN 
+(
+select city_id, max(degree) as max_degree
 from Weather
 Group by city_id
-)
-group by city_id
-order by city_id
+) m on w.city_id = m.city_id and w.degree = m.max_degree
+group by w.city_id
+order by w.city_id
