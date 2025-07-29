35c50690cd1df05f5338a609a8e050eb63bad881
Ashok Kumar <ashokkumar200194@gmail.com>
2025-07-29 02:54:05 -0500

    Time: 14 ms (31.82%), Space: 70.2 MB (22.73%) - LeetHub

---
+++ 3157-find-the-level-of-tree-with-minimum-sum/3157-find-the-level-of-tree-with-minimum-sum.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int minimumLevel(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList();
+
+        queue.offer(root);
+
+        int level = 0; 
+
+        int resLevel = 0;
+
+        long minSum = Integer.MAX_VALUE;  
+
+
+        while(!queue.isEmpty()) {
+            int len = queue.size();
+            long sum = 0;
+            level++;
+
+            while(len-- > 0) {
+                TreeNode node = queue.poll();
+                if(node != null) {
+                    sum += node.val;
+                    queue.offer(node.left);
+                    queue.offer(node.right);
+                }
+            }
+
+            if(sum != 0 && sum < minSum) {
+                minSum = sum;
+                resLevel = level;
+            }
+        }
+
+        return resLevel;
+
+    }
+}
