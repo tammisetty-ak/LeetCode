9a3991bb95df87a77ecab3868a22669bb5e77d1d
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-09 20:05:02 -0500

    Time: 10 ms (73.47%), Space: 41.8 MB (77.55%) - LeetHub

--- 1478-allocate-mailboxes/1478-allocate-mailboxes.java
+++ 1478-allocate-mailboxes/1478-allocate-mailboxes.java
@@ -6,13 +6,26 @@ class Solution {
         int n = houses.length;
         Arrays.sort(houses);
         int[][] dp = new int[n][k];
+        int[][] cost = new int[n][n];
+
         for(int i = 0; i < n; i++) {
             Arrays.fill(dp[i], -1);
         }
-        return solve(houses, 0, 0, k, dp);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                int median = houses[(i + j) / 2];
+                int sum = 0;
+                for(int l = i; l <= j; l++) {
+                    sum += Math.abs(median - houses[l]);
+                }
+                cost[i][j] = sum;
+            }
+        }
+        return solve(houses, 0, 0, k, dp, cost);
     }
 
-    private int solve(int[] houses, int pos, int currK, int k, int[][] dp) {
+    private int solve(int[] houses, int pos, int currK, int k, int[][] dp, int[][] cost) {
         if(pos == houses.length) {
             if(currK == k) {
                 return 0;
@@ -33,13 +46,7 @@ class Solution {
         int ans = max;
 
         for(int i = pos; i < houses.length; i++) {
-            int median = houses[(i + pos) / 2];
-            int cost = 0;
-            for(int j = pos; j <= i; j++) {
-                cost += Math.abs(houses[j] - median);
-            }
-
-            ans = Math.min(ans, solve(houses, i + 1, currK + 1, k, dp) + cost);
+            ans = Math.min(ans, solve(houses, i + 1, currK + 1, k, dp, cost) + cost[pos][i]);
         }
         dp[pos][currK] = ans;
         return ans;
