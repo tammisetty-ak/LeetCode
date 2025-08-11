03e0e0977f55feed45911a09fa431076ee35f2e9
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-11 15:07:40 -0500

    Time: 2 ms (66.82%), Space: 43 MB (15.01%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,51 +1,50 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        Queue<Pair<Integer, Integer>> queue = new LinkedList();
+        int m = grid.length;
+        int n = grid[0].length;
         int freshOranges = 0;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 2) {
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 2) {
                     queue.offer(new Pair(i, j));
-                } else if (grid[i][j] == 1) {
+                }
+                else if(grid[i][j] == 1){
                     freshOranges++;
                 }
             }
         }
-        // queue -> [[0,0]]
-        int[][] directions = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
-        int minLapsed = 0;
-        while (!queue.isEmpty()) {
+        int minutes = 0;
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
+        while(!queue.isEmpty()) {
             int len = queue.size();
             boolean rotten = false;
-            while (len > 0) {
+            while(len-- > 0) {
                 Pair<Integer, Integer> pair = queue.poll();
-                // [[]]
+
                 int row = pair.getKey();
                 int col = pair.getValue();
-                for (int[] d : directions) {
-                    int neighborRow = row + d[0];
-                    int neighborCol = col + d[1];
 
-                    if (neighborRow >= 0 && neighborRow < grid.length && neighborCol >= 0
-                            && neighborCol < grid[0].length) {
+                for(int[] d : directions) {
+                    int neighborRow = d[0] + row;
+                    int neighborCol = d[1] + col;
 
-                        if (grid[neighborRow][neighborCol] == 1) {
+                    if(neighborRow >= 0 && neighborRow < m && neighborCol >= 0 && neighborCol < n) {
+                        if(grid[neighborRow][neighborCol] == 1) {
                             grid[neighborRow][neighborCol] = 2;
                             freshOranges--;
                             rotten = true;
                             queue.offer(new Pair(neighborRow, neighborCol));
-                            // queue -> [[0,1] [1,0]]
                         }
                     }
+
                 }
-                len--;
             }
-            if(rotten) minLapsed++;
-
+            if(rotten)
+                    minutes++;
         }
-
-        return freshOranges == 0 ? minLapsed : -1;
+        return freshOranges != 0 ? -1 : minutes;
     }
 }
