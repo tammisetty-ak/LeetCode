b685bb242d37d180ec1711e3e68781c978359555
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-10 23:47:49 -0500

    Time: 47 ms (53.03%), Space: 102.5 MB (55.25%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.java
@@ -1,18 +1,19 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        Arrays.sort(envelopes, new Comparator<int[]> () {
-            public int compare(int[] arr1, int[] arr2) {
-                if(arr1[0] == arr2[0]) {
-                    return Integer.compare(arr2[1], arr1[1]);
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                if(a[0] == b[0]) {
+                    return Integer.compare(b[1], a[1]);
                 }
                 else {
-                    return Integer.compare(arr1[0], arr2[0]);
+                    return Integer.compare(a[0], b[0]);
                 }
             }
         });
 
-        int[] secondDim = new int[envelopes.length];
-        for(int i = 0; i < envelopes.length; i++) {
+        int n = envelopes.length;
+        int[] secondDim = new int[n];
+        for(int i = 0; i < n; i++) {
             secondDim[i] = envelopes[i][1];
         }
 
@@ -21,24 +22,38 @@ class Solution {
 
     private int LIS(int[] secondDim) {
         int n = secondDim.length;
+
         ArrayList<Integer> sub = new ArrayList<>();
         sub.add(secondDim[0]);
-
-        
         for(int i = 1; i < n; i++) {
             int num = secondDim[i];
+
             if(num > sub.get(sub.size() - 1)) {
                 sub.add(num);
             }
             else {
-                int j = 0;
-                while(sub.get(j) < num) {
-                    j++;
-                }
+                int j = binarySearch(sub, num);
                 sub.set(j, num);
             }
         }
 
         return sub.size();
+    }
+
+    private int binarySearch(ArrayList<Integer> sub, int num) {
+        int left = 0, right = sub.size() - 1;
+        while(left < right) {
+            int mid = (left + right) / 2;
+            if(sub.get(mid) == num) {
+                return mid;
+            }
+            else if(sub.get(mid) < num) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return left;
     }
 }
