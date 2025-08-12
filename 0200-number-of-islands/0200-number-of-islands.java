0ae4192caa4dad6feec52b908bd0ba0239f34aa3
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-12 03:46:27 -0500

    Time: 3 ms (82.98%), Space: 49.4 MB (72.83%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,32 @@
 class Solution {
     int ROWS, COLS;
-    int count;
-
-    private void dfs(char[][] grid, int i, int j) {
-        if(i < 0 || j < 0 || i >= ROWS || j >= COLS || grid[i][j] == '0') {
-            return;
-        }
-        grid[i][j] = '0';
-        dfs(grid, i, j + 1);
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j - 1);
-        dfs(grid, i - 1, j);
-    }
+    private static final int[][] dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
     public int numIslands(char[][] grid) {
-        ROWS = grid.length; 
+        ROWS = grid.length;
         COLS = grid[0].length;
-        count = 0;
-
+        int count = 0;
         for(int i = 0; i < ROWS; i++) {
             for(int j = 0; j < COLS; j++) {
                 if(grid[i][j] == '1') {
                     count++;
-                    dfs(grid, i, j);
+                    grid[i][j] = '0';
+                    dfs(i, j, grid);
                 }
             }
         }
         return count;
+    }
+
+    private void dfs(int i, int j, char[][] grid) {
+        for(int d[] : dir) {
+            int neighborRow = d[0] + i;
+            int neighborCol = d[1] + j;
+
+            if(neighborRow >= 0 && neighborCol < COLS && neighborCol >= 0 && neighborRow < ROWS && grid[neighborRow][neighborCol] == '1') {
+                grid[neighborRow][neighborCol] = '0';
+                dfs(neighborRow, neighborCol, grid);
+            }
+        }
     }
 }
