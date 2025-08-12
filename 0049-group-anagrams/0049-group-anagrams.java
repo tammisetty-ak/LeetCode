89ddc4b0acd847791f3dc46f0033160ffdb99e81
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-12 03:06:45 -0500

    Time: 16 ms (26.11%), Space: 48.7 MB (14.59%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,34 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> stringToAnaListMap = new HashMap();
-        
+        Map<String, List<String>> resMap = new HashMap();
+
         for(String str: strs) {
             int[] chars = new int[26];
-
-            for(char c: str.toCharArray()) {
+            for(char c : str.toCharArray()) {
                 chars[c - 'a']++;
             }
-            String key = Arrays.toString(chars);
+            
+            StringBuilder key = new StringBuilder();
 
-            if(!stringToAnaListMap.containsKey(key)) {
-                stringToAnaListMap.put(key, new ArrayList());
+            for(int i = 0; i < 26; i++) {
+                if(chars[i] != 0) {
+                    key.append((char) i + 'a');
+                    key.append(chars[i]);
+                }
             }
-
-            stringToAnaListMap.get(key).add(str);
+;
+            if(!resMap.containsKey(key.toString())) {
+                resMap.put(key.toString(), new ArrayList<String>());
+            }
+            List<String> list = resMap.get(key.toString());
+            list.add(str);
+            resMap.put(key.toString(), list);
         }
 
-        return new ArrayList<>(stringToAnaListMap.values());
-
+        List<List<String>> res = new ArrayList();
+        for(List<String> vals : resMap.values()) {
+            res.add(vals);
+        }
+        return res;
     }
 }
