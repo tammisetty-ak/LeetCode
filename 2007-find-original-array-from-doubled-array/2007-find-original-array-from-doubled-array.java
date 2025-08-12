89f86ff987adb88fed59320f8e7740071f7f8662
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-12 06:13:04 -0500

    Time: 107 ms (27.95%), Space: 64.3 MB (8.82%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,37 +1,37 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
-        if(changed.length % 2 != 0) {
-            return new int[0];
-        }
-        int max = Integer.MIN_VALUE;
-        for(int num: changed) {
-            max = Math.max(max, num);
-        }
+
+        if(changed.length % 2 == 1) {
+            return new int[]{};
+        }        
+        
+        int n = changed.length / 2;
 
 
-        int[] freq = new int[2 * max + 1];
+        Arrays.sort(changed);
+        Map<Integer, Integer> freq = new HashMap();
 
         for(int num : changed) {
-            freq[num]++;
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
-        int[] orig = new int[changed.length / 2];
-        int index = 0;
 
-        for(int i = 0; i <= max; i++) {
-            if(freq[i] > 0) {
-                freq[i]--;
-                int twice = i * 2;
+        int[] original = new int[n];
+        int i = 0;
 
-                if(freq[twice] > 0) {
-                    freq[twice]--;
-                    orig[index++] = i;
-                    i--;
+        for(int num : changed) {
+            if(freq.get(num) > 0) {
+                freq.put(num, freq.get(num) - 1);
+                int twiceNum = num * 2;
+
+                if(freq.containsKey(twiceNum) && freq.get(twiceNum) > 0) {
+                    freq.put(twiceNum, freq.get(twiceNum) - 1);
+                    original[i++] = num;
                 }
                 else {
                     return new int[0];
                 }
             }
         }
-        return orig;
+        return original;
     }
 }
