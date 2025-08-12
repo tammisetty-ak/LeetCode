a9e183090c7eafadfff6d91a8a254efc5d6cfdcc
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-12 05:45:32 -0500

    Time: 3 ms (99.6%), Space: 46.7 MB (64.03%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
+        int sum = 0;
         int min1a = Integer.MAX_VALUE, min1b = Integer.MAX_VALUE;
         int min2a = Integer.MAX_VALUE, min2b = Integer.MAX_VALUE;
-        int sum = 0;
 
-        for(int num : nums) {
-            sum += num;
+        
+        for(int num : nums){
             if(num % 3 == 1) {
                 if(num < min1a) {
                     min1b = min1a;
@@ -24,21 +24,37 @@ class Solution {
                     min2b = num;
                 }
             }
+            sum += num;
         }
 
         if(sum % 3 == 0) {
             return sum;
         }
-        
-        if(sum % 3 == 1) {
-            int op1 = min1a == Integer.MAX_VALUE ? 0 : sum - min1a;
-            int op2 = min2a != Integer.MAX_VALUE && min2b != Integer.MAX_VALUE ? sum - min2a - min2b : 0;
+
+        else if(sum % 3 == 1) {
+            int op1 = (min1a == Integer.MAX_VALUE) ? 0 : sum - min1a;
+            int op2 = (min2a == Integer.MAX_VALUE && min2b == Integer.MAX_VALUE) ? 0 : sum - min2a - min2b;
+
             return Math.max(op1, op2);
         }
+
         else {
-            int op1 = min1a != Integer.MAX_VALUE && min1b != Integer.MAX_VALUE ? sum - min1a - min1b : 0;
-            int op2 = min2a == Integer.MAX_VALUE ? 0 : sum - min2a;
+            int op1 = (min1a == Integer.MAX_VALUE) && min1b == Integer.MAX_VALUE ? 0 : sum - min1a  - min1b;
+            int op2 = (min2a == Integer.MAX_VALUE) ? 0 : sum - min2a;
             return Math.max(op1, op2);
         }
     }
 }
+
+
+// 3 6 5 1 8 -> 23 % 3 -> 2
+
+// 1 3 5 6 8 -> 1 5 8 -> 23 - max(5, 8) -> 18
+
+// 4 -> 4 -> 1
+
+// 4 -> 4
+
+// 1 2 3 4 4 -> 12 -> 
+
+// 1 2 3 4 4 -> 1 2 4 -> 8 10
