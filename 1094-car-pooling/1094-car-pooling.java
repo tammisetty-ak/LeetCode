b06c4a1d2f9ed9d502f4fdd36e1a413ae6bf2f86
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-12 02:51:54 -0500

    Time: 7 ms (29.09%), Space: 44.4 MB (45.57%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,21 +1,23 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int[] trip = new int[1001];
+        Map<Integer, Integer> passAtTime = new TreeMap();
 
-        for(int[] t : trips) {
-            trip[t[1]] += t[0];
-            trip[t[2]] -= t[0];
+        for(int[] trip : trips) {
+            int startPassengers = passAtTime.getOrDefault(trip[1], 0) + trip[0];
+            passAtTime.put(trip[1], startPassengers);
+            int endPassengers = passAtTime.getOrDefault(trip[2], 0) - trip[0];
+            passAtTime.put(trip[2], endPassengers);
         }
-
         int usedCapacity = 0;
-        for(int t: trip) {
-            if(t != 0) {
-                usedCapacity += t;
-                if(capacity < usedCapacity) {
-                    return false;
-                }
+
+        for(int cap: passAtTime.values()) {
+            usedCapacity += cap;
+
+            if(usedCapacity > capacity) {
+                return false;
             }
         }
-        return true;
+                return true;
+
     }
 }
