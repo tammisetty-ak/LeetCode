a623c63b7d048cd1335b16c6e14588908d583e27
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-12 23:23:45 -0500

    Time: 8 ms (84.41%), Space: 44 MB (83.75%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -5,9 +5,9 @@ class Solution {
         }
         while(n % 3 == 0) {
             n = n / 3;
-            if(n == 3) {
-                break;
-            }
+            // if(n == 3) {
+            //     break;
+            // }
         }
         return n == 1 || n == 3;
     }
