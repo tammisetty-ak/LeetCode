e7e3a64380e3fe7e0750e1f2efb410d12c585b85
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-14 01:47:18 -0500

    Time: 7 ms (30.55%), Space: 67.3 MB (66.72%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
@@ -1,39 +1,65 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode reverseEvenLengthGroups(ListNode head) {
-
-        ListNode temp = head;
-        int num = 1;
-
-        while(temp != null) {
-            int count = 0;
-            ListNode start = temp;
-            Stack<Integer> stack = new Stack();
-
-            while(temp != null && count != num) {
-                stack.push(temp.val);
-                temp = temp.next;
-                count++;
-            }
-
-            if(count % 2 == 0) {
-                while(temp != start) {
-                start.val = stack.pop();
-                start = start.next;
-                }
-            }
-            num++;
+        ListNode main = head;
+        ListNode temp = head.next;
+        ListNode countNodes = head;
+        int n = 0;
+        while (countNodes != null) {
+            countNodes = countNodes.next;
+            n++;
         }
+        n--;
+        int index = 2;
+        while (temp != null) {
+            ListNode list = new ListNode(0);
+            int nodeCount = 1;
 
+            if (index % 2 == 0 && n >= index) {
+
+                while (nodeCount <= index && temp != null) {
+                    ListNode temp2 = temp;
+                    temp = temp.next;
+                    temp2.next = list;
+                    list = temp2;
+                    nodeCount++;
+                    n--;
+                }
+                ListNode insert = list;
+                while (list.next.next != null) {
+                    list = list.next;
+                }
+                list.next = temp;
+                main.next = insert;
+                main = list;
+            } else {
+                if (n % 2 == 0 && n < index) {
+                    while (nodeCount <= index && temp != null) {
+                        ListNode temp2 = temp;
+                        temp = temp.next;
+                        temp2.next = list;
+                        list = temp2;
+                        nodeCount++;
+                        n--;
+                    }
+                    ListNode insert = list;
+                    while (list.next.next != null) {
+                        list = list.next;
+                    }
+                    list.next = temp;
+                    main.next = insert;
+                    main = list;
+                } else {
+                    while (nodeCount <= index && temp != null) {
+                        temp = temp.next;
+                        main = main.next;
+                        nodeCount++;
+                        n--;
+                    }
+                }
+
+            }
+            index++;
+        }
         return head;
     }
 }
