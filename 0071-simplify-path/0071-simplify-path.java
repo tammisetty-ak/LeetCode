d01598f17665564284ccea6b50763342fa715f4a
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-16 02:57:25 -0500

    Time: 20 ms (5.03%), Space: 44.7 MB (17.46%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack();
+
+        String[] fileNames = path.split("/+");
+
+        for (String fileName : fileNames) {
+            if (!fileName.isBlank()) {
+                if (!fileName.equals(".") && !fileName.equals("..")) {
+                    stack.push(fileName);
+                } else if (fileName.equals("..")) {
+                    if (!stack.isEmpty())
+                        stack.pop();
+                }
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        if(stack.isEmpty()) {
+            return "/";
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.peek());
+            res.insert(0, stack.pop());
+            res.insert(0, "/");
+        }
+
+        return res.toString();
+
+    }
+}
+
+// if(a||b){if(b&&c){
+
+// }}else{
+
+// }
