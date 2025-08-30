9ccf9f75e06ac81ce5956ac3408c6834144a511a
Ashok Kumar <ashokkumar200194@gmail.com>
2025-08-30 10:59:10 -0500

    Time: 2 ms (77.42%), Space: 44.8 MB (31.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -4,42 +4,43 @@ class Solution {
         HashSet<Character>[] colSet = new HashSet[9];
         HashSet<Character>[] boxSet = new HashSet[9];
 
-        int N = 9;
-
-        for(int i = 0; i < N; i++) {
-            rowSet[i] = new HashSet();
-            colSet[i] = new HashSet();
-            boxSet[i] = new HashSet();
+        for(int i = 0; i < 9; i++) {
+            rowSet[i] = new HashSet<Character>();
+            colSet[i] = new HashSet<Character>();
+            boxSet[i] = new HashSet<Character>();
         }
 
 
-        for(int i = 0 ; i < 9 ; i++) {
+        for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
                 char val = board[i][j];
-
                 if(val == '.') {
                     continue;
                 }
+                
                 if(rowSet[i].contains(val)) {
                     return false;
                 }
+
                 rowSet[i].add(val);
+
                 if(colSet[j].contains(val)) {
                     return false;
                 }
+
                 colSet[j].add(val);
 
-                int index = (i / 3) * 3 + (j / 3);
+                int idx = (i / 3) * 3 + (j / 3);
 
-                if(boxSet[index].contains(val)) {
+                if(boxSet[idx].contains(val)) {
                     return false;
                 }
 
-                boxSet[index].add(val);
+                boxSet[idx].add(val);
             }
         }
 
         return true;
-
+        
     }
 }
