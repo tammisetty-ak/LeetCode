4951113ffaef1dac86992d85e03ccc308cf59769
Ashok Kumar <ashokkumar200194@gmail.com>
2025-09-21 00:32:36 -0500

    Time: 0 ms (100%), Space: 52.7 MB (72.1%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int low = 0, high = nums.Length - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target) {
+                return mid;
+            }
+            else if(nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
