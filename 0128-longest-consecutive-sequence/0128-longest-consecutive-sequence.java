dc370719c5c006b612dfa10aa9777883606d8d7f
Ashok Kumar <ashokkumar200194@gmail.com>
2025-09-23 08:27:12 -0500

    Time: 112 ms (5.05%), Space: 82.5 MB (5.92%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,26 +1,46 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        HashSet<Integer> set = new HashSet();
+        if(nums.length == 0) {
+            return 0;
+        }
+        HashMap<Integer, int[]> map = new HashMap();
+        HashMap<Integer, Boolean> seen = new HashMap();
 
-        for(int n : nums) {
-            set.add(n);
+        for(int num : nums) {
+            map.putIfAbsent(num, new int[]{num - 1, num + 1});
+            seen.putIfAbsent(num, false);
         }
 
-        int longStreak = 0;
+        int count = 1;
 
-        for(int n : set) {
-            if(!set.contains(n - 1)) {
-                int currStreak = 1;
+        Queue<Integer> queue = new LinkedList<>();
+        int maxCount = 1;
 
-                while(set.contains(n + 1)) {
-                    currStreak++;
-                    n += 1;
+        for(int num : nums) {
+            queue.offer(num);
+            count = 1;
+            while(!queue.isEmpty()) {
+                int temp = queue.poll();
+                if(map.containsKey(temp) && !seen.get(temp)) {
+                    for(int neighbor : map.get(temp)) {
+                        if(map.containsKey(neighbor) && !seen.get(neighbor)) {
+                            count++;
+                            maxCount = Math.max(count, maxCount);
+                            seen.put(temp, true);
+                            queue.offer(neighbor);
+
+                        }
+                    }
                 }
-
-                longStreak = Math.max(longStreak, currStreak);
             }
         }
-
-        return longStreak;
+        return maxCount;
     }
 }
+
+// 4 [3, 5] c - 1
+// 100 [99, 101] c - 1
+//200 [199, 201] c - 1
+//1 [0, 2] c - 1
+//3 [2, 4] c - 1, c - 2
+//2 [1, 3] c - 1 c - 2 c - 3 c - 4
